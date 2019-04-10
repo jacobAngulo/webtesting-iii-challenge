8445b2164584529d1fa712619fa27a09ee76b4f0
Jacob Angulo <jacobangulo@hotmail.com>
2019-04-10 13:32:45 -0700

    initial commit

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Controls />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
-// Test away
+// Test away!
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
